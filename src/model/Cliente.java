aa6d7ffcee42dff25b4fb49905ed5011e9a4b2c8
Artur Dornfeld <artur.dornfeld@sempreceub.com>
2025-04-29 19:17:44 -0300

    Atualização das Classes

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,5 +1,52 @@
 package model;
 
 public class Cliente {
+    // Atributos
+    private String nome;
+    private String telefone;
+    private String email;
 
+    // Construtor
+    public Cliente(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Método para exibir informações do cliente
+    public void exibirInformacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Telefone: " + telefone);
+        System.out.println("E-mail: " + email);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente [nome=" + nome + ", telefone=" + telefone + ", email=" + email + "]";
+    }
 }
--- src/model/Servico.java
+++ src/model/Servico.java
@@ -1,5 +1,40 @@
 package model;
 
+/* Classe que representa um serviço disponível no Pet Shop */
 public class Servico {
+    private String tipoServico;
+    private double preco;
 
+    public Servico(String tipoServico, double preco) {
+        this.tipoServico = tipoServico;
+        this.preco = preco;
+    }
+
+    /* gets */
+    public String getTipoServico() {
+        return tipoServico;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    /* sets */
+    public void setTipoServico(String tipoServico) {
+        this.tipoServico = tipoServico;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    /* métodos */
+    public void exibirInformacoes() {
+        System.out.println("Serviço: " + tipoServico);
+        System.out.printf("Preço: R$%.2f%n", preco);
+    }
+
+    public String toString() {
+        return String.format("Servico[tipo=%s, preco=%.2f]", tipoServico, preco);
+    }
 }
